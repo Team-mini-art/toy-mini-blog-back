7abd56eb0d307f303bf1d29cd1835953e28cf687
hmmini <pak4184@naver.com>
2023-09-16 23:12:09 +0900

    [dev] findMembers apply PageAble

--- src/main/java/study/till/back/controller/MemberController.java
+++ src/main/java/study/till/back/controller/MemberController.java
@@ -1,6 +1,9 @@
 package study.till.back.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import study.till.back.dto.*;
@@ -30,8 +33,14 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public List<FindMemberResponse> findMember() {
-        memberService.findMember();
-        return memberService.findMember();
+    public List<FindMemberResponse> findMembers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "email") String sort,
+            @RequestParam(defaultValue = "asc") String direction) {
+
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sort));
+        return memberService.findMembers(pageable);
     }
 }
